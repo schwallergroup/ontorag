9c978f93212c6ffd0d878076aac267cae3b1c57a
doncamilom <doncamilom@gmail.com>
2024-09-29 17:46:59 +0200

    run several llms

--- scripts/run_biomed_bench.sh
+++ scripts/run_biomed_bench.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-python src/OntoRAG/benchmark/biomed.py \
-    --method all \
-    --ontology_path data/test/ontologies/SNOMED \
-    # --llm anthropic/claude-3-5-sonnet-20240620 \
-    --llm mistral/mistral-medium-latest \
-    --temperature 0.01 \
-    --max_tokens 512
+llms=(
+    "mistral/mistral-medium-latest"
+    # "anthropic/claude-3-5-sonnet-20240620"
+    # "openai/gpt-3.5-turbo"
+    # "openai/gpt-4-turbo-preview"
+    "openai/gpt-4o"
+    # "openai/gpt-4o-mini"
+    "groq/llama-3.1-70b-versatile"
+)
+
+for llm in "${llms[@]}"
+do
+    echo "Running benchmark with LLM: $llm"
+    python src/OntoRAG/benchmark/biomed.py \
+        --method 'ontorag-simple' \
+        --ontology_path data/ontologies/SNOMED \
+        --llm "$llm" \
+        --temperature 0.01 \
+        --max_tokens 512
+    echo "Finished running benchmark with LLM: $llm"
+    echo "----------------------------------------"
+done
