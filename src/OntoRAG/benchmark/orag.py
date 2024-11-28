c49a04f125c3be51f9177838ff1299d564a59eb0
doncamilom <doncamilom@gmail.com>
2024-11-28 15:08:14 +0100

    last update

--- src/OntoRAG/benchmark/orag.py
+++ src/OntoRAG/benchmark/orag.py
@@ -4,10 +4,11 @@ from typing import Literal, Optional, Tuple, Union
 
 import dspy
 from dotenv import load_dotenv
-from utils import qa_decorator
 
 from OntoRAG.ontorag import BaseOntoRAG
 from OntoRAG.utils import OntoRetriever
+
+from .utils import qa_decorator
 
 __all__ = [
     "SimpleORAG",
